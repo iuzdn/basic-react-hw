acc24c7601f744ffcefa3c4d2a395c11565b094a
izadnipru <iulian.zadnipru@endava.com>
2021-05-20 11:54:00 +0300

    Make todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  ListGroup,
+  Row,
+} from 'react-bootstrap';
+
+const initialTodos = [
+  { id: 1, description: 'Wash Car' },
+  { id: 2, description: 'Feed Cat' },
+  { id: 3, description: 'Shopping' },
+];
+
+const initialTodo = {
+  id: null,
+  description: '',
+};
 
 function App() {
+  const [todo, setTodo] = useState(initialTodo);
+  const [todos, setTodos] = useState(initialTodos);
+
+  const { id, description } = todo;
+
+  function createId() {
+    const newId =
+      todos.reduce((max, item) => (item.id > max ? item.id : max), 0) + 1;
+    return newId;
+  }
+
+  function handleChange(e) {
+    const updatedTodo = id
+      ? { ...todo, description: e.target.value }
+      : { id: createId(), description: e.target.value };
+    setTodo(updatedTodo);
+    console.log(todo);
+  }
+
+  function handleSubmit(e) {
+    setTodos([...todos, todo]);
+    setTodo(initialTodo);
+    e.preventDefault();
+  }
+
+  function handleDelete(id) {
+    setTodos(todos.filter(item => item.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <Form onSubmit={handleSubmit}>
+            <Form.Label>Todo input:</Form.Label>
+            <Form.Group as={Row} controlId="formBasicTodo">
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="text"
+                  value={description}
+                  placeholder="Enter todo"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Button type="submit">Add Todo</Button>
+              </Form.Group>
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Header>ToDoList</Card.Header>
+            <ListGroup>
+              {todos.map(({ id, description }) => (
+                <ListGroup.Item key={id}>
+                  <Row>
+                    <Col>{description}</Col>
+                    <Button
+                      variant="danger"
+                      className="mr-2"
+                      onClick={() => handleDelete(id)}
+                    >
+                      Delete
+                    </Button>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
